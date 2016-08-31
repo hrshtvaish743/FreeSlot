37628268ce40b9acf01a0e737bd1bc48cc434d15
Harshit <hrshtvaish743@gmail.com>
2016-08-31 18:15:54 +0530

    .

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.jpg')));
 // catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -65,7 +65,7 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
-});*/
+});
 
 
 module.exports = app;
