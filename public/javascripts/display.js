9a5a5b00b09a4f68017b628cfec0354ae6d34281
Harshit <hrshtvaish743@gmail.com>
2017-02-10 22:48:32 +0530

    Timetable display updated

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -11,7 +11,7 @@ $(document).ready(function(next) {
             for(i = 0;;i++) {
               if(!list[i]) break;
               var html = '<li><a onclick="SelectStud(' + i + ')" style="cursor:pointer;">' + list[i].name + '</a></li>';
-              $('.dropdown-menu').append(html);
+              $('#StudentDropdown').append(html);
         }
       }
     });
@@ -22,12 +22,12 @@ function SelectStud(i) {
   $('#Selected').empty().append(list[i].regno + "  " + list[i].name);
   console.log(list[i]);
   for(j = 0; j < list[i].freeslots.length; j++) {
-    $('#' + list[i].freeslots[j]).css("background-color", "green").append("Class");
+    $('#' + list[i].freeslots[j]).css("background-color", "white").empty().append("Free");
   }
 }
 
 function clear() {
   for(var i = 1; i<60; i++){
-    $('#' + i).css("background-color", "#F5F5F5").empty();
+    $('#' + i).css("background-color", "green").empty().append("Class");
   }
 }
